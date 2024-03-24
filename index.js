5684bf242c40f1f141ab31076e35f7c49bb1182e
Waseth <waseth.emmanuel@student.moringaschool.com>
2024-03-24 13:56:37 +0300

    created myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+var multiply;
+let num1=31
+let num2=2
+ var multiply=num1*num2
 
-// Write your code here
+
+ let random = Math.floor(Math.random() * 100) + 1;
+
+ let num3 = 20;
+ let num4 = 16;
+ let mod = num3 % num4;
+
+
+ const max =Math.max(5, 10, 15, 20);
+
